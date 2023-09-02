e4717f5b022654f8fcbde5ab6aae74fce0c30081
Morgan Davis <mtd37@hotmail.com>
2023-09-02 15:55:20 -0500

    Add tabs for Artists, Albums, and Genre

--- music_library/app.py
+++ music_library/app.py
@@ -46,6 +46,14 @@ def song_details(song_id):
     return render_template("song_details.html", song=song)
 
 
+@app.route("/artists")
+def artists():
+    # Extract a list of unique artist names
+    artist_names = list(set(song["artist"] for song in songs))
+    sorted_artist_names = sorted(artist_names)
+    return render_template("artists.html", artist_names=sorted_artist_names)
+
+
 @app.route("/artist/<string:artist_name>")
 def artist_details(artist_name):
     artist_songs = [song for song in songs if song["artist"] == artist_name]
@@ -61,12 +69,28 @@ def artist_details(artist_name):
     )
 
 
+@app.route("/albums")
+def albums():
+    # Extract a list of unique album names
+    album_names = list(set(song["album"] for song in songs))
+    sorted_album_names = sorted(album_names)
+    return render_template("albums.html", album_names=sorted_album_names)
+
+
 @app.route("/album/<string:album_name>")
 def album_details(album_name):
     album_songs = [song for song in songs if song["album"] == album_name]
     return render_template(
         "album_details.html", album_name=album_name, album_songs=album_songs
     )
+
+
+@app.route("/genres")
+def genres():
+    # Extract a list of unique genre names
+    genre_names = list(set(song["genre"] for song in songs))
+    sorted_genre_names = sorted(genre_names)
+    return render_template("genres.html", genre_names=sorted_genre_names)
 
 
 @app.route("/genre/<string:genre_name>")
@@ -79,7 +103,7 @@ def genre_details(genre_name):
     # Extract a list of unique album names associated with the genre
     genre_albums = list(set(song["album"] for song in sorted_genre_songs))
 
-    # TODO sort by year
+    # TODO sort albums by year
 
     # Sort the albums by title (alphabetically)
     sorted_genre_albums = sorted(genre_albums)
